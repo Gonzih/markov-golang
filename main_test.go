08fe7939ae14c025bdc869c8acc6e0930e5160d0
Max Gonzih <gonzih@gmail.com>
2017-02-24 18:00:21 +0100

    Add one more test

--- main_test.go
+++ main_test.go
@@ -20,3 +20,13 @@ func TestGenerateChain(t *testing.T) {
 		}
 	}
 }
+
+func TestGenerateOutput(t *testing.T) {
+	input := "This is super great thing."
+	chain := GenerateChain(input)
+	output := GenerateOutput(chain)
+
+	if input != output {
+		t.Errorf("%s != %s", input, output)
+	}
+}
